e80f2cf08ef3ad13f3c8d3f0e92235a1ff015857
yangbin1 <yangbin199191@163.com>
2018-11-17 16:24:11 +0800

    222

--- springDemo2/src/main/java/com/yangbin1/spring/aop/impl/CalculatorImpl.java
+++ springDemo2/src/main/java/com/yangbin1/spring/aop/impl/CalculatorImpl.java
@@ -1,29 +1,12 @@
-/**  
- * @Title:  CalculatorImpl.java   
- * @Package com.yangbin1.spring.aop.helloworld   
- * @Description:    TODO(用一句话描述该文件做�?�?)   
- * @author: yangbin1
- * @date:   Nov 12, 2018 10:42:03 AM   
- */
+
 package com.yangbin1.spring.aop.impl;
 
-/**   
- * @ClassName:  CalculatorImpl   
- * @Description:TODO(这里用一句话描述这个类的作用)   
- * @author: yangbin1
- * @date:   Nov 12, 2018 10:42:03 AM   
- *     
- */
+import org.springframework.stereotype.Component;
+
+@Component
 public class CalculatorImpl implements Calculator {
 
-    /**   
-     * <p>Title: add</p>   
-     * <p>Description: </p>   
-     * @param i
-     * @param j
-     * @return   
-     * @see com.yangbin1.spring.aop.helloworld.Calculator#add(int, int)   
-     */
+	
     @Override
     public int add(int i, int j) {
         int result = i + j;
