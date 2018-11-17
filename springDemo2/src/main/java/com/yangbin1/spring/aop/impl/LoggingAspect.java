426e610e9fde10c2c9a3a8b894a1026f0e7f38ca
yangbin1 <yangbin199191@163.com>
2018-11-17 21:39:37 +0800

    1111

--- springDemo2/src/main/java/com/yangbin1/spring/aop/impl/LoggingAspect.java
+++ springDemo2/src/main/java/com/yangbin1/spring/aop/impl/LoggingAspect.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -51,6 +53,30 @@ public class LoggingAspect {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
         System.out.println("The method " + methodName + " occurs exception: " + ex);
+    }
+    
+    //环绕通知需要ProceedingJoinPoint类型的参数
+    //环绕通知类似于动态代理里面的全过程：ProceedingJoinPoint参数决定是否执行目标方法
+    //环绕通知必须有返回值，目标方法的返回值
+    @Around(value="execution(public int com.yangbin1.spring.aop.impl.CalculatorImpl.*(int, int))")
+    public Object aroundMethod(ProceedingJoinPoint pj) {
+        Object result = null;
+        String methodName = pj.getSignature().getName();
+        // 执行目标方法
+        try {
+            // 前置通知
+            System.out.println("The method " + methodName + " begins with " + Arrays.asList(pj.getArgs()));
+            result = pj.proceed();
+            // 返回通知
+            System.out.println("The method ends with " + result);
+        } catch (Throwable e) {
+            // 异常通知
+            System.out.println("The method occurs exception " + e);
+            throw new RuntimeException(e);
+        }
+        // 后置通知
+        System.out.println("The method " + methodName + " ends");
+        return result;
     }
 
 }
