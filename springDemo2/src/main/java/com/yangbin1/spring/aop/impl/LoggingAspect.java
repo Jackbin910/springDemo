347230b1960519d18f9ed64f2f1c2c849c404b15
yangbin1 <yangbin199191@163.com>
2018-11-17 17:32:26 +0800

    222

--- springDemo2/src/main/java/com/yangbin1/spring/aop/impl/LoggingAspect.java
+++ springDemo2/src/main/java/com/yangbin1/spring/aop/impl/LoggingAspect.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,6 +20,15 @@ public class LoggingAspect {
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
         System.out.println("The method " + methodName + " begins with " + args);
+    }
+    
+    //后置通知(无论是否发生异常都通知)
+    //后置通知无法返回方法执行的结果
+    @After("execution(public int com.yangbin1.spring.aop.impl.CalculatorImpl.*(int, int))")
+    public void afterMethod(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        List<Object> args = Arrays.asList(joinPoint.getArgs());
+        System.out.println("The method " + methodName + " ends with " + args);
     }
 
 }
